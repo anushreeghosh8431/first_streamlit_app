3d1b005910d8e78264085156d74cdd41496ac17f
anushreeghosh8431 <125768869+anushreeghosh8431@users.noreply.github.com>
2023-03-04 13:54:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('omlet,bread,jam, banana')
+streamlit.text('🥣 🥗 🐔 🥑🍞.omlet,bread,jam, banana')
 streamlit.text('juice or green tea')
