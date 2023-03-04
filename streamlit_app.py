c3b643f7c0360a853d05aab5c9d6d66d650fcb55
anushreeghosh8431 <125768869+anushreeghosh8431@users.noreply.github.com>
2023-03-04 13:52:33 +0530

    Update streamlit_app.py
    menu added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omlet,bread,jam, banana')
+streamlit.text('juice or green tea')
