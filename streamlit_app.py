ee3e0b2a96a2a654c5fdb1198ca99ce5a0538733
anushreeghosh8431 <125768869+anushreeghosh8431@users.noreply.github.com>
2023-03-04 13:55:36 +0530

    Update streamlit_app.py
    new header added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 🥗 🐔 🥑🍞.omlet,bread,jam, banana')
+streamlit.text('🥣 🥗 🐔 🥑🍞.omlet,bread,jam, avocado')
 streamlit.text('juice or green tea')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
